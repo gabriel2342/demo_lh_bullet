8859b8ae1f00c056a85f52a6a0643e4438227df1
“gabriel2342” <gabriel.pendas@gmail.com>
2022-05-24 09:59:50 -0600

    Setting up marketing page

--- app/controllers/public/home_controller.rb
+++ app/controllers/public/home_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomeController < Public::ApplicationController
   # Redirect `/` to either `ENV["MARKETING_SITE_URL"]` or the sign-in page.
   # If you'd like to customize the action for `/`, you can remove this and define `def index ... end ` below.
-  include RootRedirect
+  #include RootRedirect
 
   # Allow your application to disable public sign-ups and be invitation only.
   include InviteOnlySupport
@@ -9,4 +9,10 @@ class Public::HomeController < Public::ApplicationController
   # Make Bullet Train's documentation available at `/docs`.
   include DocumentationSupport
 
+  def index
+    @municipalities = Municipality.all 
+    @service_providers = ServiceProvider.all
+  end
+
+  
 end
